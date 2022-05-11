ffb3dda8ae7f602a6911501c7e03160dfe945092
Aawez Khan <aawezk786@gmail.com>
2022-05-12 00:43:20 +0530

    Update:Helpers

---
+++ src/errors/Error.js
@@ -0,0 +1,14 @@
+class ErrorT extends Error {
+  constructor(statusCode, message) {
+    super(message);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, MyError);
+    }
+
+    this.myErr = true;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = ErrorT;
---
+++ src/errors/handleError.js
@@ -0,0 +1,22 @@
+const ErrorT = require("./Error");
+
+module.exports = (err, _req, res, next) => {
+  let error = { ...err };
+
+  error.message = err.message;
+
+  if (err.code === 11000) {
+    const message = `Duplicate Field value entered`;
+    error = new ErrorT(400, message);
+  }
+
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorT(400, message);
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Server Error",
+  });
+};
